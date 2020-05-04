7e48f965a5050b6a574beca7ebcfb655600f6c87
Aleksandra Płużek <aleksandra.pluzek@student.po.edu.pl>
2020-05-05 01:32:42 +0200

    Commit #4 (ostateczny). Moduł działa, gotowy, skończony. Nie wiem tylko co z plikami txt, z których korzysta. Są one w projekcie niby w folderze, ale czy Wam się pobiorą? Let me know.

--- Gramatyka/Gramatyka/MainWindow.xaml.cs
+++ Gramatyka/Gramatyka/MainWindow.xaml.cs
@@ -28,8 +28,55 @@ namespace Gramatyka
 
         private void Lista_rozwijana_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            if (lista_rozwijana_przeszle.SelectedItem == past_simple)
+            {
+                lista_rozwijana_terazniejsze.Text = " ";
+                lista_rozwijana_warunkowe.Text = " ";
+                lista_rozwijana_przyszle.Text = " ";
+                // Open the text file using a stream reader.
+                StreamReader sr = new StreamReader(@"past_simple.txt");
+                // Read the stream to a string, and write the string to the console.
+                String line = sr.ReadToEnd();
+                tresc.Text = line;
+            }
+            if (lista_rozwijana_przeszle.SelectedItem == past_continuous)
+            {
+                lista_rozwijana_terazniejsze.Text = " ";
+                lista_rozwijana_warunkowe.Text = " ";
+                lista_rozwijana_przyszle.Text = " ";
+                // Open the text file using a stream reader.
+                StreamReader sr = new StreamReader(@"past_continuous.txt");
+                // Read the stream to a string, and write the string to the console.
+                String line = sr.ReadToEnd();
+                tresc.Text = line;
+            }
+            if (lista_rozwijana_przeszle.SelectedItem == past_perfect)
+            {
+                lista_rozwijana_terazniejsze.Text = " ";
+                lista_rozwijana_warunkowe.Text = " ";
+                lista_rozwijana_przyszle.Text = " ";
+                // Open the text file using a stream reader.
+                StreamReader sr = new StreamReader(@"past_perfect.txt");
+                // Read the stream to a string, and write the string to the console.
+                String line = sr.ReadToEnd();
+                tresc.Text = line;
+            }
+            if (lista_rozwijana_przeszle.SelectedItem == past_perfect_continuous)
+            {
+                lista_rozwijana_terazniejsze.Text = " ";
+                lista_rozwijana_warunkowe.Text = " ";
+                lista_rozwijana_przyszle.Text = " ";
+                // Open the text file using a stream reader.
+                StreamReader sr = new StreamReader(@"past_perfect_continuous.txt");
+                // Read the stream to a string, and write the string to the console.
+                String line = sr.ReadToEnd();
+                tresc.Text = line;
+            }
             if (lista_rozwijana_terazniejsze.SelectedItem == present_simple)
             {
+                lista_rozwijana_warunkowe.Text = " ";
+                lista_rozwijana_przeszle.Text = " ";
+                lista_rozwijana_przyszle.Text = " ";
                 // Open the text file using a stream reader.
                 StreamReader sr = new StreamReader(@"present_simple.txt");
                 // Read the stream to a string, and write the string to the console.
@@ -38,6 +85,9 @@ namespace Gramatyka
             }
             if (lista_rozwijana_terazniejsze.SelectedItem == present_continuous)
             {
+                lista_rozwijana_warunkowe.Text = " ";
+                lista_rozwijana_przeszle.Text = " ";
+                lista_rozwijana_przyszle.Text = " ";
                 // Open the text file using a stream reader.
                 StreamReader sr = new StreamReader(@"present_continuous.txt");
                 // Read the stream to a string, and write the string to the console.
@@ -46,6 +96,9 @@ namespace Gramatyka
             }
             if (lista_rozwijana_terazniejsze.SelectedItem == present_perfect)
             {
+                lista_rozwijana_warunkowe.Text = " ";
+                lista_rozwijana_przeszle.Text = " ";
+                lista_rozwijana_przyszle.Text = " ";
                 // Open the text file using a stream reader.
                 StreamReader sr = new StreamReader(@"present_perfect.txt");
                 // Read the stream to a string, and write the string to the console.
@@ -54,8 +107,110 @@ namespace Gramatyka
             }
             if (lista_rozwijana_terazniejsze.SelectedItem == present_perfect_continuous)
             {
+                lista_rozwijana_warunkowe.Text = " ";
+                lista_rozwijana_przeszle.Text = " ";
+                lista_rozwijana_przyszle.Text = " ";
                 // Open the text file using a stream reader.
                 StreamReader sr = new StreamReader(@"present_perfect_continuous.txt");
+                // Read the stream to a string, and write the string to the console.
+                String line = sr.ReadToEnd();
+                tresc.Text = line;
+            }
+            if (lista_rozwijana_przyszle.SelectedItem == future_simple)
+            {
+                lista_rozwijana_terazniejsze.Text = " ";
+                lista_rozwijana_warunkowe.Text = " ";
+                lista_rozwijana_przeszle.Text = " ";
+                // Open the text file using a stream reader.
+                StreamReader sr = new StreamReader(@"future_simple.txt");
+                // Read the stream to a string, and write the string to the console.
+                String line = sr.ReadToEnd();
+                tresc.Text = line;
+            }
+            if (lista_rozwijana_przyszle.SelectedItem == future_continuous)
+            {
+                lista_rozwijana_terazniejsze.Text = " ";
+                lista_rozwijana_warunkowe.Text = " ";
+                lista_rozwijana_przeszle.Text = " ";
+                // Open the text file using a stream reader.
+                StreamReader sr = new StreamReader(@"future_continuous.txt");
+                // Read the stream to a string, and write the string to the console.
+                String line = sr.ReadToEnd();
+                tresc.Text = line;
+            }
+            if (lista_rozwijana_przyszle.SelectedItem == future_perfect)
+            {
+                lista_rozwijana_terazniejsze.Text = " ";
+                lista_rozwijana_warunkowe.Text = " ";
+                lista_rozwijana_przeszle.Text = " ";
+                // Open the text file using a stream reader.
+                StreamReader sr = new StreamReader(@"future_perfect.txt");
+                // Read the stream to a string, and write the string to the console.
+                String line = sr.ReadToEnd();
+                tresc.Text = line;
+            }
+            if (lista_rozwijana_przyszle.SelectedItem == future_perfect_continuous)
+            {
+                lista_rozwijana_terazniejsze.Text = " ";
+                lista_rozwijana_warunkowe.Text = " ";
+                lista_rozwijana_przeszle.Text = " ";
+                // Open the text file using a stream reader.
+                StreamReader sr = new StreamReader(@"future_perfect_continuous.txt");
+                // Read the stream to a string, and write the string to the console.
+                String line = sr.ReadToEnd();
+                tresc.Text = line;
+            }
+            if (lista_rozwijana_warunkowe.SelectedItem == tr_zerowy)
+            {
+                lista_rozwijana_terazniejsze.Text = " ";
+                lista_rozwijana_przeszle.Text = " ";
+                lista_rozwijana_przyszle.Text = " ";
+                // Open the text file using a stream reader.
+                StreamReader sr = new StreamReader(@"zero_conditional.txt");
+                // Read the stream to a string, and write the string to the console.
+                String line = sr.ReadToEnd();
+                tresc.Text = line;
+            }
+            if (lista_rozwijana_warunkowe.SelectedItem == tr_pierwszy)
+            {
+                lista_rozwijana_terazniejsze.Text = " ";
+                lista_rozwijana_przeszle.Text = " ";
+                lista_rozwijana_przyszle.Text = " ";
+                // Open the text file using a stream reader.
+                StreamReader sr = new StreamReader(@"first_conditional.txt");
+                // Read the stream to a string, and write the string to the console.
+                String line = sr.ReadToEnd();
+                tresc.Text = line;
+            }
+            if (lista_rozwijana_warunkowe.SelectedItem == tr_drugi)
+            {
+                lista_rozwijana_terazniejsze.Text = " ";
+                lista_rozwijana_przeszle.Text = " ";
+                lista_rozwijana_przyszle.Text = " ";
+                // Open the text file using a stream reader.
+                StreamReader sr = new StreamReader(@"second_conditional.txt");
+                // Read the stream to a string, and write the string to the console.
+                String line = sr.ReadToEnd();
+                tresc.Text = line;
+            }
+            if (lista_rozwijana_warunkowe.SelectedItem == tr_trzeci)
+            {
+                lista_rozwijana_terazniejsze.Text = " ";
+                lista_rozwijana_przeszle.Text = " ";
+                lista_rozwijana_przyszle.Text = " ";
+                // Open the text file using a stream reader.
+                StreamReader sr = new StreamReader(@"third_conditional.txt");
+                // Read the stream to a string, and write the string to the console.
+                String line = sr.ReadToEnd();
+                tresc.Text = line;
+            }
+            if (lista_rozwijana_warunkowe.SelectedItem == tr_mieszany)
+            {
+                lista_rozwijana_terazniejsze.Text = " ";
+                lista_rozwijana_przeszle.Text = " ";
+                lista_rozwijana_przyszle.Text = " ";
+                // Open the text file using a stream reader.
+                StreamReader sr = new StreamReader(@"mixed_conditional.txt");
                 // Read the stream to a string, and write the string to the console.
                 String line = sr.ReadToEnd();
                 tresc.Text = line;
